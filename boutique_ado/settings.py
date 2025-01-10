f8a2a0c1c2e672edd0bdbb8ae89fd3380443a84f
Kenya Francis <kenyarae99@gmail.com>
2025-01-10 13:12:30 +0000

    Fixing deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r30n@-(c5p3n@1s$*i^%u@k9&4cv2n@o$xz5&meu2h8**-j*ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kenya-rae-boutique-ado-codealong.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [' https://boutique-ado-codealong-10ea193a624f.herokuapp.com', 'localhost']
 
 
 # Application definition
